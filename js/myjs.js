73fbbc446a114f324d1dd433f4f07f950e97892a
Ben Singer <benjaminjsinger@gmail.com>
2015-01-29 20:57:35 -0600

    initial commit

---
+++ js/myjs.js
@@ -0,0 +1,19 @@
+var comm = new Icecomm('NPxgHL3vfw3Lq7wchKeWZaYm056GAZo8rZjFKXcynTZNzVHhi');
+
+comm.connect('custom room', {audio: false});
+
+comm.on('connected', function(options) {
+  console.log(options);
+  var row = document.getElementById('row');
+  var td = document.createElement('td');
+  td.appendChild(options.video);
+  row.appendChild(td);
+});
+
+comm.on('local', function(options) {
+  localVideo.src = options.stream;
+});
+
+comm.on('disconnect', function(options) {
+  document.getElementById(options.callerID).remove();
+});
