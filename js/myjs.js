63eb7826eec0329e506b8f548439237b39088d09
Ben Singer <benjamin.singer@helpsystems.com>
2015-02-06 15:50:29 -0600

    added chat and removed resizing for guest videos

--- js/myjs.js
+++ js/myjs.js
@@ -1,7 +1,8 @@
 
 var comm = new Icecomm('NPxgHL3vfw3Lq7wchKeWZaYm056GAZo8rZjFKXcynTZNzVHhi');
-comm.connect('custom room', {audio: true});
+comm.connect('customroom123', {audio: true});
 
+var z  = 10;
 comm.on('connected', function(options) {
     console.log(options);
     var c = document.getElementById('container');
@@ -10,6 +11,10 @@ comm.on('connected', function(options) {
     div.appendChild(el);
     div.setAttribute("id", options.callerID);
     div.className = "resizeDiv draggable";
+    div.onmousedown = function(){
+    	z += 1;
+    	this.style.zIndex = z;
+    };
     var draggie = new Draggabilly(div, {
       containment: '#container'
     });
@@ -23,4 +28,30 @@ comm.on('local', function(options) {
 comm.on('disconnect', function(options) {
     var el = document.getElementById(options.callerID);
     el.parentNode.removeChild(el);
+});
+
+var feed = document.getElementById("feed");
+comm.on('data', function(options){	
+	
+	var isScrolledToBottom = feed.scrollHeight - feed.clientHeight <= feed.scrollTop + 1;
+	var p = document.createElement('p');
+	p.innerHTML = "<b>" + options.callerID.replace("-","") + "</b><br>" + options.data + "<hr>";
+	feed.appendChild(p);
+	if(isScrolledToBottom) feed.scrollTop = feed.scrollHeight - feed.clientHeight;
+});
+
+$('textarea').on('keydown', function(e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        if(comm.getLocalID() && e.currentTarget.value.length > 0){        
+	        comm.send(e.currentTarget.value);
+			var isScrolledToBottom = feed.scrollHeight - feed.clientHeight <= feed.scrollTop + 1;
+			var p = document.createElement('p');
+			p.innerHTML = "<b>Me</b><br>" + e.currentTarget.value + "<hr>";
+			feed.appendChild(p);
+	        if(isScrolledToBottom) 
+	        	feed.scrollTop = feed.scrollHeight - feed.clientHeight;
+	        e.currentTarget.value = "";
+    	}
+    }
 });
