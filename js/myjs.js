fbf0aa7bcb9ca214a6fa5eef92e5b1aedfb92802
Ben Singer <benjamin.singer@helpsystems.com>
2015-07-16 14:18:57 -0500

    using angular now

--- js/myjs.js
+++ js/myjs.js
@@ -1,89 +1,117 @@
-var QueryString = function () {
-  // This function is anonymous, is executed immediately and
-  // the return value is assigned to QueryString!
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-        // If first entry with this name
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-        // If second entry with this name
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-      query_string[pair[0]] = arr;
-        // If third or later entry with this name
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+var QueryString = function() {
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = decodeURIComponent(pair[1]);
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+            query_string[pair[0]] = arr;
+        } else {
+            query_string[pair[0]].push(decodeURIComponent(pair[1]));
+        }
     }
-  }
     return query_string;
 }();
 
-var comm = new Icecomm('OEPs1Gvg8qHtrE8q3OniMGdYUxqScxZ5gXbE7lHr4guAugJk');
-var roomName;
-if(QueryString.room){
-    roomName = QueryString.room.replace('/','');
-}else{
-    var roomName = prompt("You are the host! Please enter a room name:");
-    document.location.search = "?room=" + roomName;
-}
 
+var GitChat = angular.module('GitChat', []);
 
-comm.connect(roomName, {audio: true});
+GitChat.controller('ChatController', ["$scope", function($scope) {
+    var self = this;
+    self.peers = [];
+    self.messages = [];
 
-var z  = 10;
-comm.on('connected', function(peer) {
-
-    var c = document.getElementById('container');
-    var el = peer.getVideo();
-    var div = document.createElement('div');
-    div.appendChild(el);
-    div.setAttribute("id", peer.callerID);
-    div.className = "resizeDiv draggable";
-    div.onmousedown = function(){
-    	z += 1;
-    	this.style.zIndex = z;
-    };
-    var draggie = new Draggabilly(div, {
-      containment: '#container'
-    });
-    c.appendChild(div);
-});
-
-comm.on('local', function(options) {
-    console.log("roomName", roomName);
-  	localVideo.src = options.stream;
-});
-
-comm.on('disconnect', function(options) {
-    var el = document.getElementById(options.callerID);
-    el.parentNode.removeChild(el);
-});
-
-var feed = document.getElementById("feed");
-comm.on('data', function(options){
-	var isScrolledToBottom = feed.scrollHeight - feed.clientHeight <= feed.scrollTop + 1;
-	var p = document.createElement('p');
-    var name = options.data.name ? options.data.name : options.callerID.replace("-","");
-	p.innerHTML = "<b class=\"peerMessage\">" + name + "</b><br>" + Autolinker.link(options.data.msg) + "<hr>";
-	feed.appendChild(p);
-	if(isScrolledToBottom) feed.scrollTop = feed.scrollHeight - feed.clientHeight;
-});
-
-$('textarea').on('keydown', function(e) {
-    if (e.which == 13) {
-        e.preventDefault();
-        if(comm.getLocalID() && e.currentTarget.value.length > 0){
-	        comm.send({ name: $('#name').val(), msg: e.currentTarget.value });
-			var isScrolledToBottom = feed.scrollHeight - feed.clientHeight <= feed.scrollTop + 1;
-			var p = document.createElement('p');
-			p.innerHTML = "<b>Me</b><br>" + Autolinker.link(e.currentTarget.value) + "<hr>";
-			feed.appendChild(p);
-	        if(isScrolledToBottom)
-	        	feed.scrollTop = feed.scrollHeight - feed.clientHeight;
-	        e.currentTarget.value = "";
-    	}
+    var comm = new Icecomm('OEPs1Gvg8qHtrE8q3OniMGdYUxqScxZ5gXbE7lHr4guAugJk');
+    var roomName;
+    if (QueryString.room) {
+        roomName = QueryString.room.replace('/', '');
+    } else {
+        roomName = prompt("No room specified to join! Please enter a new room name:");
+        document.location.search = "?room=" + roomName;
     }
+
+    comm.connect(roomName, {
+        audio: true
+    });
+
+    comm.on('connected', function(peer) {
+        console.log(peer, peer.getVideo());
+        self.peers.push(peer);
+        $scope.$apply();
+    });
+
+    comm.on('local', function(options) {
+        localVideo.src = options.stream;
+    });
+
+    comm.on('disconnect', function(peer) {
+
+        for (var i = 0; i < self.peers.length; i++) {
+            if (self.peers[i].callerID === peer.callerID) {
+                self.peers.splice(i, 1);
+                break;
+            }
+        }
+        $scope.$apply();
+    });
+
+    comm.on('data', function(options) {
+        self.scrollFeed(function(){
+            if (options.data.msg) {
+                self.messages.push(options.data);
+                $scope.$apply();
+            }
+        });
+    });
+
+    self.sendMessage = function() {
+        self.scrollFeed(function(){
+            if (self.message.length > 0) {
+                var msg = {
+                    name: self.name,
+                    msg: self.message
+                };
+                comm.send(msg);
+                msg.name = "Me";
+                self.messages.push(msg);
+                self.message = "";
+            }
+        });
+    };
+
+
+    self.scrollFeed = function(callback){
+        $scope.$evalAsync(function() {
+            var feed = $('#feed');
+            // var isScrolledToBottom = feed[0].scrollHeight - feed[0].clientHeight <= feed[0].scrollTop + 1;
+            // console.log(isScrolledToBottom);
+            callback();
+            // if(isScrolledToBottom) feed[0].scrollTop = feed[0].scrollHeight - feed[0].clientHeight;
+            setTimeout(function(){
+                feed[0].scrollTop = feed[0].scrollHeight - feed[0].clientHeight;
+            }, 100);
+        });
+    };
+}]);
+
+GitChat.filter('trustUrl', function($sce) {
+    return function(url) {
+        return $sce.trustAsResourceUrl(url);
+    };
+});
+
+GitChat.directive('ngEnter', function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if (event.which === 13) {
+                scope.$apply(function() {
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+            }
+        });
+    };
 });
